290a04d02b948a26f73160ffbe2e7e221d61d48e
TP@黒猫亭 <tp@kuronekotei.net>
2017-01-24 00:42:07 +0900

    tpNpcChat v1.2.0

--- tpNpcChat/src/tpnpcchat.lua
+++ tpNpcChat/src/tpnpcchat.lua
@@ -30,12 +30,22 @@ end
 
 --	再読込しても、この関数(の効果)は更新されない(DIALOG_SHOW_DIALOG_TEXTになってるため)
 function TPNPCCHAT_HOOK_DIALOG_SHOW_DIALOG_TEXT(frame, text, titleName, voiceName)
-	TPNPCCHAT_NEW_DIALOG_SHOW_DIALOG_TEXT(frame, text, titleName, voiceName);
+	local f,m = pcall(TPNPCCHAT_NEW_DIALOG_SHOW_DIALOG_TEXT,frame, text, titleName, voiceName);
+	if f then
+		-- あえて何も書かない
+	else
+		CHAT_SYSTEM(m);
+	end
 	TPNPCCHAT_OLD_DIALOG_SHOW_DIALOG_TEXT(frame, text, titleName, voiceName);
 end
 
 function TPNPCCHAT_HOOK_NOTICE_ON_MSG(frame, msg, argStr, argNum)
-	TPNPCCHAT_NEW_NOTICE_ON_MSG(frame, msg, argStr, argNum);
+	local f,m = pcall(TPNPCCHAT_NEW_NOTICE_ON_MSG,frame, msg, argStr, argNum);
+	if f then
+		-- あえて何も書かない
+	else
+		CHAT_SYSTEM(m);
+	end
 	TPNPCCHAT_OLD_NOTICE_ON_MSG(frame, msg, argStr, argNum);
 end
 
@@ -47,6 +57,10 @@ function TPNPCCHAT_LOAD_SETTING()
 	s1.isShowScr = s1.isShowScr or true; -- NOTICE_Dm_scroll
 	s1.isShowClr = s1.isShowClr or true; -- NOTICE_Dm_Clear
 	s1.isShowGet = s1.isShowGet or true; -- NOTICE_Dm_GetItem
+	s1.isShowRes = s1.isShowRes or true; -- NOTICE_Dm_ResBuff!
+	s1.isShowUse = s1.isShowUse or true; -- NOTICE_Dm_UsePotion!
+	s1.isShowCmp = s1.isShowCmp or true; -- NOTICE_Dm_quest_complete!
+	s1.isShowGlb = s1.isShowGlb or true; -- NOTICE_Dm_UsePotion!
 	s1.isShowLvl = s1.isShowLvl or true; -- NOTICE_Dm_levelup_base NOTICE_Dm_levelup_skill
 end
 
@@ -77,32 +91,58 @@ function TPNPCCHAT_NEW_NOTICE_ON_MSG(frame, msg, argStr, argNum)
 
 	if (msg == "NOTICE_Dm_!") and s1.isShowExc then
 		if (dispMode ==1) then
-			CHAT_SYSTEM("{img NOTICE_Dm_! "         ..iconSize.." "..iconSize.."}{/}{/}{#FF2000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_! "              ..iconSize.." "..iconSize.."}{/}{/}{#FF2000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		else
-			CHAT_SYSTEM("{img NOTICE_Dm_! "         ..iconSize.." "..iconSize.."}{/}{/}{#E00000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_! "              ..iconSize.." "..iconSize.."}{/}{/}{#E00000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		end
 	elseif (msg == "NOTICE_Dm_scroll") and s1.isShowScr then
 		if (dispMode ==1) then
-			CHAT_SYSTEM("{img NOTICE_Dm_scroll "    ..iconSize.." "..iconSize.."}{/}{/}{#E04000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_scroll "         ..iconSize.." "..iconSize.."}{/}{/}{#E04000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		else
-			CHAT_SYSTEM("{img NOTICE_Dm_scroll "    ..iconSize.." "..iconSize.."}{/}{/}{#602000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_scroll "         ..iconSize.." "..iconSize.."}{/}{/}{#602000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		end
 	elseif (msg == "NOTICE_Dm_Clear") and s1.isShowClr then
 		if (dispMode ==1) then
-			CHAT_SYSTEM("{img NOTICE_Dm_Clear "     ..iconSize.." "..iconSize.."}{/}{/}{#00A0FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_Clear "          ..iconSize.." "..iconSize.."}{/}{/}{#00A0FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		else
-			CHAT_SYSTEM("{img NOTICE_Dm_Clear "     ..iconSize.." "..iconSize.."}{/}{/}{#0000FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_Clear "          ..iconSize.." "..iconSize.."}{/}{/}{#0000FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		end
 	elseif (msg == "NOTICE_Dm_GetItem") and s1.isShowGet then
 		if (dispMode ==1) then
-			CHAT_SYSTEM("{img NOTICE_Dm_GetItem "   ..iconSize.." "..iconSize.."}{/}{/}{#00A0FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_GetItem "        ..iconSize.." "..iconSize.."}{/}{/}{#00A0FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		else
-			CHAT_SYSTEM("{img NOTICE_Dm_GetItem "   ..iconSize.." "..iconSize.."}{/}{/}{#0000FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+			CHAT_SYSTEM("{img NOTICE_Dm_GetItem "        ..iconSize.." "..iconSize.."}{/}{/}{#0000FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
 		end
 	elseif (msg == "NOTICE_Dm_levelup_base") and s1.isShowLvl then
-		CHAT_SYSTEM("{img NOTICE_Dm_levelup_base "  ..iconSize.." "..iconSize.."}{/}{/}{s"..fontSize.."}" .. argStr.."{nl}");
+		CHAT_SYSTEM("{img NOTICE_Dm_levelup_base "       ..iconSize.." "..iconSize.."}{/}{/}{s"..fontSize.."}" .. argStr.."{nl}");
 	elseif (msg == "NOTICE_Dm_levelup_skill") and s1.isShowLvl then
-		CHAT_SYSTEM("{img NOTICE_Dm_levelup_skill " ..iconSize.." "..iconSize.."}{/}{/}{s"..fontSize.."}" .. argStr.."{nl}");
+		CHAT_SYSTEM("{img NOTICE_Dm_levelup_skill "      ..iconSize.." "..iconSize.."}{/}{/}{s"..fontSize.."}" .. argStr.."{nl}");
+	elseif (msg == "NOTICE_Dm_ResBuff") and s1.isShowRes then
+		if (dispMode ==1) then
+			CHAT_SYSTEM("{img NOTICE_Dm_ResBuff! "       ..iconSize.." "..iconSize.."}{/}{/}{#FF2000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		else
+			CHAT_SYSTEM("{img NOTICE_Dm_ResBuff! "       ..iconSize.." "..iconSize.."}{/}{/}{#E00000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		end
+	elseif (msg == "NOTICE_Dm_UsePotion") and s1.isShowUse then
+		if (dispMode ==1) then
+			CHAT_SYSTEM("{img NOTICE_Dm_UsePotion! "     ..iconSize.." "..iconSize.."}{/}{/}{#FF2000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		else
+			CHAT_SYSTEM("{img NOTICE_Dm_UsePotion! "     ..iconSize.." "..iconSize.."}{/}{/}{#E00000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		end
+	elseif (msg == "NOTICE_Dm_quest_complete") and s1.isShowCmp then
+		if (dispMode ==1) then
+			CHAT_SYSTEM("{img NOTICE_Dm_quest_complete " ..iconSize.." "..iconSize.."}{/}{/}{#00A0FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		else
+			CHAT_SYSTEM("{img NOTICE_Dm_quest_complete " ..iconSize.." "..iconSize.."}{/}{/}{#0000FF}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		end
+	elseif (msg == "NOTICE_Dm_Global_Shout") and s1.isShowGlb then
+		if (dispMode ==1) then
+			CHAT_SYSTEM("{img NOTICE_Dm_Global_Shout! "  ..iconSize.." "..iconSize.."}{/}{/}{#FF2000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		else
+			CHAT_SYSTEM("{img NOTICE_Dm_Global_Shout! "  ..iconSize.." "..iconSize.."}{/}{/}{#E00000}{s"..fontSize.."}" .. argStr.."{/}{/}{nl}");
+		end
+	elseif (msg == "NOTICE_Dm_GuildQuestSuccess") then
+		--何もしない
 	elseif s1.isDebug then
 		CHAT_SYSTEM("{#FF2000}"..msg.."{nl}"..argStr.."{/}{nl} ");
 	end
