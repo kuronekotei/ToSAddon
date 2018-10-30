7d0d4b435c2aa6edee413c3884885dd5d121cd2d
TP@黒猫亭 <tp@kuronekotei.net>
2018-10-30 23:35:27 +0900

    tpFarmed v1.0.0

--- tpFarmed/README.md
+++ tpFarmed/README.md
@@ -1,23 +1,28 @@
 # tpFarmed
-手に入れた物に関するログが出ます。テスト版
+手に入れた物に関するログが出ます。  
+v1.0.0でダメージ集計に対応しました。
 
 
 - マップ切り替え時に前マップの取得経験とシルバー表示(無い物は出ません)
 - アイテム取得時に個数と総個数を表示  
   重ならないものは個数が出ません
 - アイテム取得時に冒険日誌の進捗表示  
-  カウントは疑似カウントです、本来カウントされない取得方法でも加算表示されます。
+  カウントは疑似カウントです、本来カウントされない取得方法でも加算表示されます
+- ダメージ集計は対応するログの設定がONでないと集計されません  
+  ログの設定はキャラ毎なので注意
 
 
 設定あります。  
-「/addons/tpfarmed/settings.json」をテキストで編集してください。
+「/addons/tpfarmed/stg_tpfarmed.json」をテキストで編集してください。
 
 | 名称 | 効果 |
 | ---- | ---- |
 | isShowCube	 | true/false　falseだとキューブ開封時の専用ログが出なくなります。	 |
 | isShowSilver	 | true/false　falseだとシルバー取得時のログが出なくなります。	 |
-| ManyMoney		 | true/false　この価格を超えると、上記がfalseでも表示されます。	 |
 | isShowJournal	 | true/false　falseだと冒険日誌用のカウントが出なくなります。	 |
+| isShowGiveDmg	 | true/false　falseだと与ダメージ集計が出なくなります。	 |
+| isShowTakeDmg	 | true/false　falseだと受ダメージ集計が出なくなります。	 |
+| ManyMoney		 | true/false　この価格を超えると、上記がfalseでも表示されます。	 |
 
 
 
--- tpFarmed/tpfarmed/tpfarmed.lua
+++ tpFarmed/tpfarmed/tpfarmed.lua
@@ -30,6 +30,11 @@ function TPFARMED_ON_INIT(addon, frame)
 		g4.TPFARMED_OLD_GACHA_CUBE_SUCEECD_UI = GACHA_CUBE_SUCEECD_UI;
 		_G["GACHA_CUBE_SUCEECD_UI"] = TPFARMED_HOOK_GACHA_CUBE_SUCEECD_UI;
 	end
+	if(g4.TPFARMED_OLD_CHAT_SYSTEM==nil) then
+		--	待避する関数がすでにいたら、やらない　(2度置き換えると無限ループ)
+		g4.TPFARMED_OLD_CHAT_SYSTEM = CHAT_SYSTEM;
+		_G["CHAT_SYSTEM"] = TPFARMED_CHAT_SYSTEM;
+	end
 	addon:RegisterMsg("GAME_START", "TPFARMED_GAME_START")
 	addon:RegisterMsg("INV_ITEM_IN", "TPFARMED_INV_ITEM_IN");
 	addon:RegisterMsg("JOB_EXP_UPDATE", "TPFARMED_JOB_EXP_UPDATE");
@@ -46,6 +51,17 @@ function TPFARMED_HOOK_GACHA_CUBE_SUCEECD_UI(frame, invItemClsID, rewardItem, bt
 		CHAT_SYSTEM(m);
 	end
 	g4.TPFARMED_OLD_GACHA_CUBE_SUCEECD_UI(frame, invItemClsID, rewardItem, btnVisible);
+end
+
+function TPFARMED_CHAT_SYSTEM(msg)
+	if (msg == nil) then
+		return;
+	end
+	local chk1,chk2 = msg:find("!@#$Get{ITEM}{COUNT}");
+	if (chk1 ~= nil) and (chk1 > 0) then
+		return;
+	end
+	g4.TPFARMED_OLD_CHAT_SYSTEM(msg);
 end
 
 function TPFARMED_GAME_START(frame, control)
@@ -77,6 +93,8 @@ function g4.TPFARMED_LOAD_SETTING()
 	s4.isShowCube		= ((type(s4.isShowCube		) == "boolean")	and s4.isShowCube		)or (s4.isShowCube		==nil);
 	s4.isShowSilver		= ((type(s4.isShowSilver	) == "boolean")	and s4.isShowSilver		)or (s4.isShowSilver	==nil);
 	s4.isShowJournal	= ((type(s4.isShowJournal	) == "boolean")	and s4.isShowJournal	)or (s4.isShowJournal	==nil);
+	s4.isShowGiveDmg	= ((type(s4.isShowGiveDmg	) == "boolean")	and s4.isShowGiveDmg	)or (s4.isShowGiveDmg	==nil);
+	s4.isShowTakeDmg	= ((type(s4.isShowTakeDmg	) == "boolean")	and s4.isShowTakeDmg	)or (s4.isShowTakeDmg	==nil);
 	s4.ManyMoney		= ((type(s4.ManyMoney		) == "number")	and s4.ManyMoney		)or 10000;
 end
 
@@ -88,6 +106,8 @@ function g4.TPFARMED_SAVE_SETTING()
 		filep:write(",\t\"isShowCube\":"	.. ((s4.isShowCube		and "true") or "false")	.."\n"	);
 		filep:write(",\t\"isShowSilver\":"	.. ((s4.isShowSilver	and "true") or "false")	.."\n"	);
 		filep:write(",\t\"isShowJournal\":"	.. ((s4.isShowJournal	and "true") or "false")	.."\n"	);
+		filep:write(",\t\"isShowGiveDmg\":"	.. ((s4.isShowGiveDmg	and "true") or "false")	.."\n"	);
+		filep:write(",\t\"isShowTakeDmg\":"	.. ((s4.isShowTakeDmg	and "true") or "false")	.."\n"	);
 		filep:write(",\t\"ManyMoney\":"		.. (s4.ManyMoney					or 10000)	.."\n"	);
 		filep:write("}\n");
 		filep:close();
@@ -196,6 +216,7 @@ function g4.TPFARMED_EXP_DUMP()	--	未使用
 end
 
 function g4.TPFARMED_MAPSTART()
+	--CHAT_SYSTEM("TPFARMED_MAPSTART");
 	--	マップIDの取得
 	local mapName = session.GetMapName();
 	local mapProp = geMapTable.GetMapProp(mapName);
@@ -235,22 +256,110 @@ function g4.TPFARMED_MAPSTART()
 		local expCX = expCT+expCN - g4.StartCExp;
 		local moneyX = money - g4.StartMoney;
 		if (expBX > 0) then
-			CHAT_SYSTEM("{#FFFE80}{s14}{ol}　＋BExp：" .. g4.lpnts(expBX,14) .. "{/}{/}{/}");
+			CHAT_SYSTEM("{#FFFE80}{s14}{ol}　＋BExp：" .. g4.lpnts(expBX,15) .. "{/}{/}{/}");
 		end
 		if (expCX > 0) then
-			CHAT_SYSTEM("{#FFFE80}{s14}{ol}　＋CExp：" .. g4.lpnts(expCX,14) .. "{/}{/}{/}");
+			CHAT_SYSTEM("{#FFFE80}{s14}{ol}　＋CExp：" .. g4.lpnts(expCX,15) .. "{/}{/}{/}");
 		end
 		if (moneyX > 0) then
-			CHAT_SYSTEM("{#FFFE80}{s14}{ol}　＋{img icon_item_silver 14 14}　：" .. g4.lpnts(moneyX,14) .. "{/}{/}{/}");
+			CHAT_SYSTEM("{#FFFE80}{s14}{ol}　＋{img icon_item_silver 14 14}　：" .. g4.lpnts(moneyX,15) .. "{/}{/}{/}");
+		end
+		if s4.isShowGiveDmg and (g4.MemGiveDmg ~= nil) then
+			for key,val in pairs(g4.MemGiveDmg) do
+				CHAT_SYSTEM("{#FFFE80}{s14}{ol}　◇与Dm：" .. g4.lpnts(val,15) .. "：".. key .. "{/}{/}{/}");
+			end
+		end
+		if s4.isShowTakeDmg and (g4.MemTakeDmg ~= nil) then
+			for key,val in pairs(g4.MemTakeDmg) do
+				CHAT_SYSTEM("{#FFFE80}{s14}{ol}　◆被Dm：" .. g4.lpnts(val,15) .. "：".. key .. "{/}{/}{/}");
+			end
 		end
 	end
-	g4.CharName = charName
-	g4.MapName = mapName;
-	g4.MapNameS = mapNameS;
-	g4.StartBExp = expBT+expBN;
-	g4.StartCExp = expCT+expCN;
-	g4.StartMoney = money;
+	g4.CharName		= charName
+	g4.MapName		= mapName;
+	g4.MapNameS		= mapNameS;
+	g4.StartBExp	= expBT+expBN;
+	g4.StartCExp	= expCT+expCN;
+	g4.StartMoney	= money;
+	g4.MemLogSize	= g4.MemLogSize or 0;
+	g4.MemMsgId		= g4.MemMsgId or 0;
+	g4.MemGiveDmg	= {};
+	g4.MemTakeDmg	= {};
+	local frm = ui.GetFrame("tpfarmed");
+	frm:ShowWindow(1);
+	frm:RunUpdateScript("TPFARMED_UPDATE",  1, 0.0, 0, 1);
 	CHAT_SYSTEM("{#FFFE80}{s14}{ol}▽" .. mapNameS .. " [" .. mapName .. "]{/}{/}{/}");
+end
+function TPFARMED_UPDATE(frame)
+	local f,m = pcall(g4.TPFARMED_UPDATE);
+	if f ~= true then
+		CHAT_SYSTEM(m);
+	elseif (m == 1) then
+		return 1;	--	RunUpdateScriptは1で継続
+	end
+	return 0;	--	RunUpdateScriptは1で継続
+end
+function g4.TPFARMED_UPDATE()
+	local logsize = session.ui.GetMsgInfoSize("chatgbox_TOTAL");
+	if (logsize ~= nil) and (logsize ~= g4.MemLogSize) then
+		g4.MemLogSize = logsize;
+		local logpt = logsize-1;
+		if logpt > 0 then
+			local msg1stInfo = session.ui.GetChatMsgInfo("chatgbox_TOTAL", logpt);
+			local msg1stId = msg1stInfo:GetMsgInfoID();
+			while logpt > 0 do
+				local msgInfo = session.ui.GetChatMsgInfo("chatgbox_TOTAL", logpt);
+				local msgId = msgInfo:GetMsgInfoID();
+				if (g4.MemMsgId == msgId) then
+					break;
+				end
+				local msgType = msgInfo:GetMsgType();
+				if (msgType == "Battle") then
+					local tempMsg = msgInfo:GetMsg();
+					tempMsg = tempMsg:sub(1,1000);
+					if s4.isShowGiveDmg then
+						local chk1,chk2 = tempMsg:find("GiveDamage{TO}{AMOUNT}");
+						if chk1 ~= nil and chk1 > 0 then
+							local chk3,chk4 = tempMsg:find("%$TO%$%*%$");
+							local chkmon = tempMsg:sub(chk4+1);
+							local chk5,chk6 = chkmon:find("%$%*%$AMOUNT%$%*%$");
+							local chk7,chk8 = chkmon:find("#@!");
+							if chk7 ~= nil and chk7 > 0 then
+								local chknum = chkmon:sub(chk6+1,chk7-1);
+								chkmon = chkmon:sub(1,chk5-1);
+								chknum = chknum:gsub(",","");
+								if (chkmon ~= nil) and (chknum ~= nil) and (tonumber(chknum) ~= nil) and (tonumber(chknum) > 0) then
+									g4.MemGiveDmg[chkmon] = (g4.MemGiveDmg[chkmon] or 0) + tonumber(chknum);
+									-- CHAT_SYSTEM(""..chkmon.." /"..chknum.." /"..g4.MemGiveDmg[chkmon].." x"..msgId);
+								end
+							end
+						end
+					end
+					if s4.isShowTakeDmg then
+						local chk1,chk2 = tempMsg:find("TakeDamage{FROM}{AMOUNT}");
+						if chk1 ~= nil and chk1 > 0 then
+							local chk3,chk4 = tempMsg:find("%$FROM%$%*%$");
+							local chkmon = tempMsg:sub(chk4+1);
+							local chk5,chk6 = chkmon:find("%$%*%$AMOUNT%$%*%$");
+							local chk7,chk8 = chkmon:find("#@!");
+							if chk7 ~= nil and chk7 > 0 then
+								local chknum = chkmon:sub(chk6+1,chk7-1);
+								chkmon = chkmon:sub(1,chk5-1);
+								chknum = chknum:gsub(",","");
+								if (chkmon ~= nil) and (chknum ~= nil) and (tonumber(chknum) ~= nil) and (tonumber(chknum) > 0) then
+									g4.MemTakeDmg[chkmon] = (g4.MemTakeDmg[chkmon] or 0) + tonumber(chknum);
+									-- CHAT_SYSTEM(""..chkmon.." /"..chknum.." /"..g4.MemTakeDmg[chkmon].." x"..msgId);
+								end
+							end
+						end
+					end
+				end
+				logpt = logpt -1;
+			end
+			g4.MemMsgId = msg1stId;
+		end
+	end
+	return 1;	--	RunUpdateScriptは1で継続
 end
 
 function g4.TPFARMED_NEW_GACHA_CUBE_SUCEECD_UI(frame, invItemClsID, rewardItem, btnVisible)
@@ -310,23 +419,18 @@ end
 
 function g4.nts(num)
 	local numStr		= ""..num;
-	if (#numStr > 9) then
-		numStr = string.sub(numStr,0,#numStr-9)..","..string.sub(numStr,#numStr-8,#numStr-6)..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
+	if (#numStr > 12) then
+		numStr = string.sub(numStr,0,#numStr-12)..","..string.sub(numStr,#numStr-11,#numStr-9)..","..string.sub(numStr,#numStr-8,#numStr-6)..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
+	elseif (#numStr > 9) then
+		numStr = string.sub(numStr,0,#numStr-9)                                               ..","..string.sub(numStr,#numStr-8,#numStr-6)..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
 	elseif (#numStr > 6) then
-		numStr = string.sub(numStr,0,#numStr-6)..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
+		numStr = string.sub(numStr,0,#numStr-6)                                                                                            ..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
 	elseif (#numStr > 3) then
-		numStr = string.sub(numStr,0,#numStr-3)..","..string.sub(numStr,#numStr-2);
+		numStr = string.sub(numStr,0,#numStr-3)                                                                                                                                         ..","..string.sub(numStr,#numStr-2);
 	end
 	return numStr;
 end
 function g4.lpnts(num,len)
-	local numStr		= ""..num;
-	if (#numStr > 9) then
-		numStr = string.sub(numStr,0,#numStr-9)..","..string.sub(numStr,#numStr-8,#numStr-6)..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
-	elseif (#numStr > 6) then
-		numStr = string.sub(numStr,0,#numStr-6)..","..string.sub(numStr,#numStr-5,#numStr-3)..","..string.sub(numStr,#numStr-2);
-	elseif (#numStr > 3) then
-		numStr = string.sub(numStr,0,#numStr-3)..","..string.sub(numStr,#numStr-2);
-	end
+	local numStr		= g4.nts(num);
 	return string.rep(" ", len - #numStr) .. numStr;
 end
