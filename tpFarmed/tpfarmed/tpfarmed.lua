03fe7651b4fb19dedd84b96c3d7f3a67084f181a
TP@黒猫亭 <tp@kuronekotei.net>
2018-11-02 23:14:17 +0900

    tpFarmed v1.1.0

--- tpFarmed/tpfarmed/tpfarmed.lua
+++ tpFarmed/tpfarmed/tpfarmed.lua
@@ -10,12 +10,6 @@ g4.settingPath = g4.settingpath or "../addons/tpfarmed/stg_tpfarmed.json";
 g4.settings = g4.settings or {};
 local s4 = g4.settings;
 
-ADDONS											= ADDONS or {};
-ADDONS.torahamu									= ADDONS.torahamu or {};
-ADDONS.torahamu.CHATEXTENDS						= ADDONS.torahamu.CHATEXTENDS or {};
-ADDONS.torahamu.CHATEXTENDS.settings			= ADDONS.torahamu.CHATEXTENDS.settings or {};
-ADDONS.torahamu.CHATEXTENDS.settings.BALLON_FLG	= ADDONS.torahamu.CHATEXTENDS.settings.BALLON_FLG or false;
-
 function TPFARMED_ON_INIT(addon, frame)
 	local f,m = pcall(g4.TPFARMED_LOAD_SETTING);
 	if f ~= true then
@@ -38,6 +32,7 @@ function TPFARMED_ON_INIT(addon, frame)
 	addon:RegisterMsg("GAME_START", "TPFARMED_GAME_START")
 	addon:RegisterMsg("INV_ITEM_IN", "TPFARMED_INV_ITEM_IN");
 	addon:RegisterMsg("JOB_EXP_UPDATE", "TPFARMED_JOB_EXP_UPDATE");
+	addon:RegisterMsg("JOB_EXP_ADD", "TPFARMED_JOB_EXP_UPDATE");
 	local f,m = pcall(g4.TPFARMED_INIT);
 	if f ~= true then
 		CHAT_SYSTEM(m);
@@ -71,6 +66,13 @@ function TPFARMED_GAME_START(frame, control)
 	end
 end
 
+function TPFARMED_UI_GAME_START(frame, control)
+	local f,m = pcall(g4.TPFARMED_UI_GAME_START, frame, control);
+	if f ~= true then
+		CHAT_SYSTEM(m);
+	end
+end
+
 function TPFARMED_INV_ITEM_IN(frame, msg, guid, num)
 	local f,m = pcall(g4.TPFARMED_GETITEM, frame, msg, guid, num);
 	if f ~= true then
@@ -82,6 +84,17 @@ function TPFARMED_JOB_EXP_UPDATE(frame, msg, str, exp, tableinfo)
 	g4.JobLvl = tableinfo.level;
 	g4.JobExp = exp - tableinfo.startExp;
 end
+
+function TPFARMED_UI_LBUTTONUP()
+	local f,m = pcall(g4.TPFARMED_UI_LBUTTONUP);
+	if f ~= true then
+		CHAT_SYSTEM(m);
+	end
+end
+
+
+
+
 
 function g4.TPFARMED_LOAD_SETTING()
 	local t, err = acutil.loadJSON(g4.settingPath);
@@ -95,7 +108,13 @@ function g4.TPFARMED_LOAD_SETTING()
 	s4.isShowJournal	= ((type(s4.isShowJournal	) == "boolean")	and s4.isShowJournal	)or (s4.isShowJournal	==nil);
 	s4.isShowGiveDmg	= ((type(s4.isShowGiveDmg	) == "boolean")	and s4.isShowGiveDmg	)or (s4.isShowGiveDmg	==nil);
 	s4.isShowTakeDmg	= ((type(s4.isShowTakeDmg	) == "boolean")	and s4.isShowTakeDmg	)or (s4.isShowTakeDmg	==nil);
+	s4.isShowExpGain	= ((type(s4.isShowExpGain	) == "boolean")	and s4.isShowExpGain	)or false;
+	s4.isShowPopCnt		= ((type(s4.isShowPopCnt	) == "boolean")	and s4.isShowPopCnt		)or false;
+	s4.isShowTimeTbl	= ((type(s4.isShowTimeTbl	) == "boolean")	and s4.isShowTimeTbl	)or false;
 	s4.ManyMoney		= ((type(s4.ManyMoney		) == "number")	and s4.ManyMoney		)or 10000;
+	s4.useUI			= ((type(s4.useUI			) == "boolean")	and s4.useUI			)or (s4.useUI			==nil);
+	s4.posX				= ((type(s4.posX			) == "number")	and s4.posX				)or 400;
+	s4.posY				= ((type(s4.posY			) == "number")	and s4.posY				)or 400;
 end
 
 function g4.TPFARMED_SAVE_SETTING()
@@ -108,7 +127,13 @@ function g4.TPFARMED_SAVE_SETTING()
 		filep:write(",\t\"isShowJournal\":"	.. ((s4.isShowJournal	and "true") or "false")	.."\n"	);
 		filep:write(",\t\"isShowGiveDmg\":"	.. ((s4.isShowGiveDmg	and "true") or "false")	.."\n"	);
 		filep:write(",\t\"isShowTakeDmg\":"	.. ((s4.isShowTakeDmg	and "true") or "false")	.."\n"	);
+		filep:write(",\t\"isShowTimeTbl\":"	.. ((s4.isShowTimeTbl	and "true") or "false")	.."\n"	);
+		filep:write(",\t\"isShowExpGain\":"	.. ((s4.isShowExpGain	and "true") or "false")	.."\n"	);
+		filep:write(",\t\"isShowPopCnt\":"	.. ((s4.isShowPopCnt	and "true") or "false")	.."\n"	);
 		filep:write(",\t\"ManyMoney\":"		.. (s4.ManyMoney					or 10000)	.."\n"	);
+		filep:write(",\t\"useUI\":"			.. ((s4.useUI			and "true") or "false")	.."\n"	);
+		filep:write(",\t\"posX\":"			.. (s4.posX							or 400)		.."\n"	);
+		filep:write(",\t\"posY\":"			.. (s4.posY							or 400)		.."\n"	);
 		filep:write("}\n");
 		filep:close();
 	end
@@ -145,7 +170,7 @@ function g4.TPFARMED_INIT()
 	end
 	local clsList, cnt = GetClassList("Xp_Job");	
 	
-	for i=1, 140 do
+	for i=1, 154 do
 		local classR	= math.floor((i-1)/14)+1;
 		local classL	= (i % 14 == 0) and 14 or (i % 14);
 		local className	= "Job_" .. classR .. "_" ..  classL;
@@ -201,6 +226,7 @@ function g4.TPFARMED_EXP_DUMP()	--	未使用
 	CHAT_SYSTEM("R" .. g4.lpnts(clsR,2) .. "L" .. g4.lpnts(clsL,2).." "..g4.lpnts(expCN,14).."/"..g4.lpnts(expC,14));
 	CHAT_SYSTEM("Total  "..g4.lpnts(expCT+expCN,14).."/"..g4.lpnts(expCR,14));
 		
+	local i = 0;
 	for i=1, g4.ExpTableBLen do
 		local expB  = g4.ExpTableB[i]["Req"];
 		local expBT = g4.ExpTableB[i]["Ttl"];
@@ -214,7 +240,6 @@ function g4.TPFARMED_EXP_DUMP()	--	未使用
 	--	CHAT_SYSTEM("R" .. g4.lpnts(clsR,2) .. "L" .. g4.lpnts(clsL,2).."   Ttl:"..g4.lpnts(expCT,14).."   Req:"..g4.lpnts(expC,14));
 	end
 end
-
 function g4.TPFARMED_MAPSTART()
 	--CHAT_SYSTEM("TPFARMED_MAPSTART");
 	--	マップIDの取得
@@ -283,83 +308,335 @@ function g4.TPFARMED_MAPSTART()
 	g4.StartMoney	= money;
 	g4.MemLogSize	= g4.MemLogSize or 0;
 	g4.MemMsgId		= g4.MemMsgId or 0;
+	g4.TtlGiveDmg	= 0;
+	g4.TtlTakeDmg	= 0;
 	g4.MemGiveDmg	= {};
 	g4.MemTakeDmg	= {};
+	g4.TimeTable	= {};
+	g4.TimeSec		= 0;
+	g4.FirstClock	= 0;
+	g4.LastClock	= 0;
+	g4.MemPopCnt	= 0;
+	g4.PopMax		= 0;
+	g4.PopCnt		= 0;
 	local frm = ui.GetFrame("tpfarmed");
 	frm:ShowWindow(1);
 	frm:RunUpdateScript("TPFARMED_UPDATE",  1, 0.0, 0, 1);
 	CHAT_SYSTEM("{#FFFE80}{s14}{ol}▽" .. mapNameS .. " [" .. mapName .. "]{/}{/}{/}");
 end
 function TPFARMED_UPDATE(frame)
+	-- タイムテーブルと、経験値の更新
+	local f,m = pcall(g4.TPFARMED_TIMETABLE);
+	if f ~= true then
+		CHAT_SYSTEM(m);
+	end
+	-- Ch人数の把握
+	local f,m = pcall(g4.TPFARMED_POPCOUNT);
+	if f ~= true then
+		CHAT_SYSTEM(m);
+	end
+	-- ダメージログの集計
 	local f,m = pcall(g4.TPFARMED_UPDATE);
 	if f ~= true then
 		CHAT_SYSTEM(m);
-	elseif (m == 1) then
-		return 1;	--	RunUpdateScriptは1で継続
 	end
-	return 0;	--	RunUpdateScriptは1で継続
+	-- UIの更新
+	local f,m = pcall(g4.TPFARMED_UI_UPDATE);
+	if f ~= true then
+		CHAT_SYSTEM(m);
+	end
+	return 1;	--	RunUpdateScriptは1で継続
 end
+
+function g4.TPFARMED_UI_GAME_START(frame, control)
+	local frm	= ui.GetFrame("tpfarmed_ui");
+	if (frm == nil) then
+		return;
+	end
+	frm:MoveFrame(s4.posX		,s4.posY	);
+	local boxdps	= tolua.cast(frm:GetChild("boxdps"), "ui::CGroupBox");
+	if (boxdps ~= nil) then
+		boxdps:SetColorTone("C0000000");
+	end
+	local boxbex	= tolua.cast(frm:GetChild("boxbex"), "ui::CGroupBox");
+	if (boxbex ~= nil) then
+		boxbex:SetColorTone("C0000000");
+	end
+	local boxcex	= tolua.cast(frm:GetChild("boxcex"), "ui::CGroupBox");
+	if (boxcex ~= nil) then
+		boxcex:SetColorTone("C0000000");
+	end
+	local boxpop	= tolua.cast(frm:GetChild("boxpop"), "ui::CGroupBox");
+	if (boxpop ~= nil) then
+		boxpop:SetColorTone("C0000000");
+	end
+	if s4.useUI then
+		frm:ShowWindow(1);
+	else
+		frm:ShowWindow(0);
+	end
+end
+
+function g4.TPFARMED_UI_UPDATE()
+	local frm	= ui.GetFrame("tpfarmed_ui");
+	if (frm == nil) then
+		return;
+	end
+	local timdps = math.min(g4.LastClock - g4.FirstClock ,50)+g4.TimeSec;
+	local ttldps =0;
+	for i=1, 6 do
+		ttldps = ttldps + (g4.TimeTable["X"..i].GiveDmg or 0);
+	end
+	local numdps = math.floor(ttldps/timdps);
+	local strdps ="{#FE8080}DPS :"..g4.lpnts(numdps,15).."{/}/sec in"..g4.lpnts(timdps,3).."sec   {#FEC080}MapTotal:"..g4.lpnts(g4.TtlGiveDmg,15).."{/}";
+
+	--	キャラオブジェクトの取得
+	local pcObj = GetMyPCObject();
+
+	--	現在経験値の取得
+	local pcLv = pcObj.Lv;
+	local expBR = g4.ExpTableB[pcLv]["Req"] or 0;
+	local expBN = session.GetEXP();
+
+	--	現在クラス経験値の取得
+	local clsL  = g4.JobLvl;
+	local clsR  = session.GetPcTotalJobGrade()
+	local clLv = (clsR-1) *14 + clsL;
+	local expCR = g4.ExpTableC[clLv]["Req"] or 0;
+	local expCN = g4.JobExp;
+	local perbex = expBN*100/(expBR+1);
+	local percex = expCN*100/(expCR+1);
+	expBR = math.floor(expBR /1000);
+	expBN = math.floor(expBN /1000);
+	expCR = math.floor(expCR /1000);
+	expCN = math.floor(expCN /1000);
+	
+	local strbex ="{#8080FE}BExp:"..g4.lpnts(expBN,11).."K{/}/{#80FEFE}"..g4.lpnts(expBR,11).."K{/} {#80FE80}"..string.format("%5.2f", perbex).."%{/}";
+	local strcex ="{#8080FE}CExp:"..g4.lpnts(expCN,11).."K{/}/{#80FEFE}"..g4.lpnts(expCR,11).."K{/} {#80FE80}"..string.format("%5.2f", percex).."%{/}";
+
+	local strpop ="POP:"..g4.lpnts(g4.PopCnt,3).."/"..g4.lpnts(g4.PopMax,3);
+
+	local boxdps	= tolua.cast(frm:GetChild("boxdps"), "ui::CGroupBox");
+	if (boxdps ~= nil) then
+		local txtdps	= tolua.cast(boxdps:GetChild("txtdps"), "ui::CRichText");
+		if (txtdps ~= nil) then
+			txtdps:SetText(strdps);
+		end
+	end
+
+	local boxbex	= tolua.cast(frm:GetChild("boxbex"), "ui::CGroupBox");
+	if (boxbex ~= nil) then
+		local txtbex	= tolua.cast(boxbex:GetChild("txtbex"), "ui::CRichText");
+		if (txtbex ~= nil) then
+			txtbex:SetText(strbex);
+		end
+	end
+
+	local boxcex	= tolua.cast(frm:GetChild("boxcex"), "ui::CGroupBox");
+	if (boxcex ~= nil) then
+		local txtcex	= tolua.cast(boxcex:GetChild("txtcex"), "ui::CRichText");
+		if (txtcex ~= nil) then
+			txtcex:SetText(strcex);
+		end
+	end
+
+	local boxpop	= tolua.cast(frm:GetChild("boxpop"), "ui::CGroupBox");
+	if (boxpop ~= nil) then
+		if (s4.isShowPopCnt) then
+			boxpop:ShowWindow(1);
+			local txtpop	= tolua.cast(boxpop:GetChild("txtpop"), "ui::CRichText");
+			if (txtpop ~= nil) then
+				txtpop:SetText(strpop);
+			end
+		else
+			boxpop:ShowWindow(0);
+		end
+	end
+end
+
+function g4.TPFARMED_UI_LBUTTONUP()
+	local frm	= ui.GetFrame("tpfarmed_ui");
+	if (frm == nil) then
+		return;
+	end
+	s4.posX	= frm:GetX();
+	s4.posY	= frm:GetY();
+	g4.TPFARMED_SAVE_SETTING();
+end
+
+function g4.TPFARMED_TIMETABLE()
+	local nowTime	= math.floor(os.clock());
+	local nowTmP10	= math.floor(nowTime/10)*10;
+	local nowTmM10	= math.floor(nowTime%10);
+	g4.FirstClock	= g4.FirstClock or nowTime;
+
+	--	キャラオブジェクトの取得
+	local pcObj = GetMyPCObject();
+
+	--	現在経験値の取得
+	local pcLv = pcObj.Lv;
+	local expBT = g4.ExpTableB[pcLv]["Ttl"];
+	local expBN = session.GetEXP();
+
+	--	現在クラス経験値の取得
+	local clsL  = g4.JobLvl;
+	local clsR  = session.GetPcTotalJobGrade()
+	local clLv = (clsR-1) *14 + clsL;
+	local expCT = g4.ExpTableC[clLv]["Ttl"];
+	local expCN = g4.JobExp;
+
+	local money = GET_TOTAL_MONEY();
+
+	if (g4.LastClock ~= nowTmP10) then	--	10秒に1回動く
+
+		if (s4.isShowTimeTbl) and (s4.isShowGiveDmg) and (g4.LastClock ~= 0) and (g4.TimeTable["X1"].GiveDmg > 0) then
+			CHAT_SYSTEM("{#80FE80}{s14}{ol}　Dmg/10Sec:"..g4.lpnts(g4.TimeTable["X1"].GiveDmg,15).." (@"..g4.lpnts(nowTime,7) .. "){/}{/}{/}");
+		end
+		local i = 0;
+		for i=1, 29 do
+			g4.TimeTable["X"..(31-i)] = (g4.TimeTable["X"..(30-i)] or {});
+		end
+		g4.TimeTable["X1"] = {};
+		g4.TimeTable["X1"].GiveDmg = 0;
+		g4.TimeTable["X1"].TakeDmg = 0;
+		g4.TimeTable["X1"].expBS = expBT+expBN;
+		g4.TimeTable["X1"].expCS = expCT+expCN;
+		g4.TimeTable["X1"].moneyS = money;
+		g4.TimeTable["X1"].expBX  = 0;
+		g4.TimeTable["X1"].expCX  = 0;
+		g4.TimeTable["X1"].moneyX = 0;
+		if (g4.LastClock ~= 0) then	--	リアルタイムに更新される値はこのタイミングで確定させる
+			g4.TimeTable["X2"].expBX  = g4.TimeTable["X1"].expBS  - g4.TimeTable["X2"].expBS;
+			g4.TimeTable["X2"].expCX  = g4.TimeTable["X1"].expCS  - g4.TimeTable["X2"].expCS;
+			g4.TimeTable["X2"].moneyX = g4.TimeTable["X1"].moneyS - g4.TimeTable["X2"].moneyS;
+			if (g4.isShowTimeTbl) and (g4.isShowExpGain) and (g4.LastClock ~= 0) and (g4.TimeTable["X2"].expBX > 0) or (g4.TimeTable["X2"].expCX > 0) then
+				CHAT_SYSTEM("{#80FE80}{s14}{ol}　Exp/10Sec:"..g4.lpnts(g4.TimeTable["X2"].expBX,15) .." /" ..g4.lpnts(g4.TimeTable["X2"].expCX,15) .." (@"..g4.lpnts(nowTime,7) .. "){/}{/}{/}");
+			end
+			if (g4.isShowTimeTbl) and (g4.isShowSilver) and (g4.LastClock ~= 0) and (g4.LastClock ~= 0) and (g4.TimeTable["X2"].moneyX > 0) then
+				CHAT_SYSTEM("{#80FE80}{s14}{ol}　Sil/10Sec:"..g4.lpnts(g4.TimeTable["X2"].moneyX,15) .." (@"..g4.lpnts(nowTime,7) .. "){/}{/}{/}");
+			end
+		end
+	end
+	g4.LastClock = nowTmP10;
+	g4.TimeSec = nowTmM10;
+
+	g4.TimeTable["X1"].expBX  = (expBT+expBN) - g4.TimeTable["X1"].expBS;
+	g4.TimeTable["X1"].expCX  = (expCT+expCN) - g4.TimeTable["X1"].expCS;
+	g4.TimeTable["X1"].moneyX = money         - g4.TimeTable["X1"].moneyS;
+end
+
+function g4.TPFARMED_POPCOUNT()
+	if (s4.isShowPopCnt) or (g4.MemPopCnt == 0) then
+		local channel = session.loginInfo.GetChannel();
+		local zoneInsts = session.serverState.GetMap();
+		local popMax = session.serverState.GetMaxPCCount();
+		if zoneInsts == nil or zoneInsts:GetZoneInstCount() == 0 then
+			return;
+		end
+		app.RequestChannelTraffics();
+		local zoneInst = zoneInsts:GetZoneInstByIndex(channel);
+		g4.PopMax = popMax;
+		g4.PopCnt = zoneInst.pcCount;
+		if (g4.MemPopCnt ~= zoneInst.pcCount) and ((g4.MemPopCnt < 5) or (zoneInst.pcCount < 5)) then
+			g4.MemPopCnt = zoneInst.pcCount;
+			CHAT_SYSTEM("{#C0FF80}{s14}{ol}　□POP " .. zoneInst.pcCount .. "/" .. popMax .. "{/}{/}{/}");
+		end
+	end
+end
+
 function g4.TPFARMED_UPDATE()
 	local logsize = session.ui.GetMsgInfoSize("chatgbox_TOTAL");
-	if (logsize ~= nil) and (logsize ~= g4.MemLogSize) then
+	if (logsize ~= nil) and (logsize > 0) then
 		g4.MemLogSize = logsize;
 		local logpt = logsize-1;
 		if logpt > 0 then
 			local msg1stInfo = session.ui.GetChatMsgInfo("chatgbox_TOTAL", logpt);
 			local msg1stId = msg1stInfo:GetMsgInfoID();
 			while logpt > 0 do
-				local msgInfo = session.ui.GetChatMsgInfo("chatgbox_TOTAL", logpt);
-				local msgId = msgInfo:GetMsgInfoID();
-				if (g4.MemMsgId == msgId) then
-					break;
-				end
-				local msgType = msgInfo:GetMsgType();
-				if (msgType == "Battle") then
-					local tempMsg = msgInfo:GetMsg();
-					tempMsg = tempMsg:sub(1,1000);
-					if s4.isShowGiveDmg then
-						local chk1,chk2 = tempMsg:find("GiveDamage{TO}{AMOUNT}");
-						if chk1 ~= nil and chk1 > 0 then
-							local chk3,chk4 = tempMsg:find("%$TO%$%*%$");
-							local chkmon = tempMsg:sub(chk4+1);
-							local chk5,chk6 = chkmon:find("%$%*%$AMOUNT%$%*%$");
-							local chk7,chk8 = chkmon:find("#@!");
-							if chk7 ~= nil and chk7 > 0 then
-								local chknum = chkmon:sub(chk6+1,chk7-1);
-								chkmon = chkmon:sub(1,chk5-1);
-								chknum = chknum:gsub(",","");
-								if (chkmon ~= nil) and (chknum ~= nil) and (tonumber(chknum) ~= nil) and (tonumber(chknum) > 0) then
-									g4.MemGiveDmg[chkmon] = (g4.MemGiveDmg[chkmon] or 0) + tonumber(chknum);
-									-- CHAT_SYSTEM(""..chkmon.." /"..chknum.." /"..g4.MemGiveDmg[chkmon].." x"..msgId);
-								end
-							end
-						end
-					end
-					if s4.isShowTakeDmg then
-						local chk1,chk2 = tempMsg:find("TakeDamage{FROM}{AMOUNT}");
-						if chk1 ~= nil and chk1 > 0 then
-							local chk3,chk4 = tempMsg:find("%$FROM%$%*%$");
-							local chkmon = tempMsg:sub(chk4+1);
-							local chk5,chk6 = chkmon:find("%$%*%$AMOUNT%$%*%$");
-							local chk7,chk8 = chkmon:find("#@!");
-							if chk7 ~= nil and chk7 > 0 then
-								local chknum = chkmon:sub(chk6+1,chk7-1);
-								chkmon = chkmon:sub(1,chk5-1);
-								chknum = chknum:gsub(",","");
-								if (chkmon ~= nil) and (chknum ~= nil) and (tonumber(chknum) ~= nil) and (tonumber(chknum) > 0) then
-									g4.MemTakeDmg[chkmon] = (g4.MemTakeDmg[chkmon] or 0) + tonumber(chknum);
-									-- CHAT_SYSTEM(""..chkmon.." /"..chknum.." /"..g4.MemTakeDmg[chkmon].." x"..msgId);
-								end
-							end
-						end
-					end
+				local f,m = pcall(g4.TPFARMED_UPDATEROW,logpt,logsize);
+				if f ~= true then
+					CHAT_SYSTEM(m);
+				elseif (m) then
+					break; --trueを返すとループ終了
 				end
 				logpt = logpt -1;
 			end
 			g4.MemMsgId = msg1stId;
 		end
 	end
-	return 1;	--	RunUpdateScriptは1で継続
+	return;
+end
+
+function g4.TPFARMED_UPDATEROW(logpt,logsize)
+	local msgInfo = session.ui.GetChatMsgInfo("chatgbox_TOTAL", logpt);
+	if (msgInfo == nil) then
+		CHAT_SYSTEM("Alert:MsgInfoNil");
+		return false; --trueを返すとループ終了
+	end
+	local msgId = msgInfo:GetMsgInfoID();
+	local chkLogSize = session.ui.GetMsgInfoSize("chatgbox_TOTAL");
+	if (chkLogSize ~= logsize) then
+		if (logpt < chkLogSize) then
+			logpt = chkLogSize;
+		end
+		CHAT_SYSTEM("Alert:SizeChange "..logsize.." /"..chkLogSize);
+	end
+	if (g4.MemMsgId == msgId) then
+		return true; --trueを返すとループ終了
+	end
+	local msgType = msgInfo:GetMsgType();
+	if (msgType == "Battle") then
+		local tempMsg = dictionary.ReplaceDicIDInCompStr(msgInfo:GetMsg());
+		if s4.isShowGiveDmg then
+			local chk1,chk2 = tempMsg:find("GiveDamage{TO}{AMOUNT}");
+			if (chk1 ~= nil) and (chk1 > 0) then
+				local chk3,chk4 = tempMsg:find("%$TO%$%*%$");
+				local chkmon = tempMsg:sub(chk4+1);
+				local chk5,chk6 = chkmon:find("%$%*%$AMOUNT%$%*%$");
+				local chk7,chk8 = chkmon:find("#@!");
+				if (chk6 ~= nil) and (chk6 > 0) and (chk7 ~= nil) and (chk7 > 0) then
+					local chknum = chkmon:sub(chk6+1,chk7-1);
+					chkmon = chkmon:sub(1,chk5-1);
+					chknum = chknum:gsub(",","");
+					if (chkmon ~= nil) and (chknum ~= nil) and (tonumber(chknum) ~= nil) and (tonumber(chknum) > 0) then
+						g4.MemGiveDmg[chkmon] = (g4.MemGiveDmg[chkmon] or 0) + tonumber(chknum);
+						g4.TimeTable["X1"].GiveDmg = g4.TimeTable["X1"].GiveDmg + tonumber(chknum);
+						g4.TtlGiveDmg = g4.TtlGiveDmg + tonumber(chknum);
+						-- CHAT_SYSTEM(""..chkmon.." /"..chknum.." /"..g4.MemGiveDmg[chkmon].." x"..msgId);
+					else
+						CHAT_SYSTEM("Alert:GiveDamage2 "..tempMsg);
+					end
+				else
+					CHAT_SYSTEM("Alert:GiveDamage1 "..tempMsg);
+				end
+			end
+		end
+		if s4.isShowTakeDmg then
+			local chk1,chk2 = tempMsg:find("TakeDamage{FROM}{AMOUNT}");
+			if (chk1 ~= nil) and (chk1 > 0) then
+				local chk3,chk4 = tempMsg:find("%$FROM%$%*%$");
+				local chkmon = tempMsg:sub(chk4+1);
+				local chk5,chk6 = chkmon:find("%$%*%$AMOUNT%$%*%$");
+				local chk7,chk8 = chkmon:find("#@!");
+				if (chk6 ~= nil) and (chk6 > 0) and (chk7 ~= nil) and (chk7 > 0) then
+					local chknum = chkmon:sub(chk6+1,chk7-1);
+					chkmon = chkmon:sub(1,chk5-1);
+					chknum = chknum:gsub(",","");
+					if (chkmon ~= nil) and (chknum ~= nil) and (tonumber(chknum) ~= nil) and (tonumber(chknum) > 0) then
+						g4.MemTakeDmg[chkmon] = (g4.MemTakeDmg[chkmon] or 0) + tonumber(chknum);
+						g4.TimeTable["X1"].TakeDmg = g4.TimeTable["X1"].TakeDmg + tonumber(chknum);
+						g4.TtlTakeDmg = g4.TtlTakeDmg + tonumber(chknum);
+						-- CHAT_SYSTEM(""..chkmon.." /"..chknum.." /"..g4.MemTakeDmg[chkmon].." x"..msgId);
+					else
+						CHAT_SYSTEM("Alert:TakeDamage2 "..tempMsg);
+					end
+				else
+					CHAT_SYSTEM("Alert:TakeDamage1 "..tempMsg);
+				end
+			end
+		end
+	end
+	return false; --trueを返すとループ終了
 end
 
 function g4.TPFARMED_NEW_GACHA_CUBE_SUCEECD_UI(frame, invItemClsID, rewardItem, btnVisible)
